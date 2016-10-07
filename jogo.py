6d00a8ab434b4dda0c097d075b339dadcb1d807f
Aislan Jefferson <aislanjsb@gmail.com>
2016-10-07 15:59:42 -0300

    Inserido cabecalho com informacoes gerais sobre o projeto

---
+++ jogo.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""Jogo da velha - Aula GIT - PEM
+
+Descricao:
+    Jogo da velha em interface grafica em linha de comando
+     com dois modos: Contra o computador (utilizando o algoritmo minimax)
+     e contra um adversario via rede.
+TODO:
+    -Tudo!
+
+Autores:
+    Aislan Jefferson - aislanjsb@gmail.com
+    Douglas Nickson - douglas.nickson1@gmail.com
+    Laerty Santos - laerty.santos@gmail.com
+    Willian Klein - williannene1@hotmail.com
+
+"""
+
+
+
