61ad5718ce5b36a7f7b9d3a09d4533a2c20b7500
Aislan Jefferson <aislanjsb@gmail.com>
2016-10-16 16:36:05 -0300

    Inserido tag de codificacao utf-8

--- jogo.py
+++ jogo.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """Jogo da velha - Aula GIT - PEM
 
 Descricao:
--- jogo_cliente.py
+++ jogo_cliente.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """Jogo da velha - Aula GIT - PEM
 
 Descricao:
